7f69d21a1616975fbeca9562aa48842ac649f73f
Andy Tsang <atsang36@hotmail.com>
2020-07-19 16:51:31 -0700

    Updates

---
+++ precache-manifest.b1ff58d88fe959a80352570bfcf279fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734b740b42f83b121c9b8fbf3674577f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "882244de5a8f7d67bf4e",
+    "url": "/robofriends/static/css/main.dfbd2939.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882244de5a8f7d67bf4e",
+    "url": "/robofriends/static/js/main.4d1162d7.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
